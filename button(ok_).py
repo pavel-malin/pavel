83af0703c21ae95077f1bbd50bb43a7adaae486e
malinroot <kurchevskijpavel@gmail.com>
2018-06-04 16:58:38 +0300

    Create button(ok_).py

---
+++ button(ok_).py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+button1 = Button(root, text='ok', width=25, height=5, bg='black' ,fg='red', font='arial 14')
+button1.pack()
+root.mainloop()
+
+"""
+
+Button ok (color black and red), with the appearance of the mouse becomes white, and when the layer becomes black.
+For beginners very well you can do before for the application or in the program for the product.
+
+"""
